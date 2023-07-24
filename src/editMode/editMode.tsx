919436d983ec784d5db25ffded00d77298abd3a7
goxi <dotaplayf@gmail.com>
2023-07-24 19:01:59 +0200

    fixed bug in comments and profile

--- src/editMode/editMode.tsx
+++ src/editMode/editMode.tsx
@@ -19,6 +19,32 @@ const EditModeComponent = () => {
     const profileRef = useRef<HTMLDivElement>(null);
     const commentRef = useRef<HTMLDivElement>(null);
     const textareaRef = useRef<HTMLTextAreaElement>(null);
+    const handleSetProfile = () => {
+        if (profileValue && profileValue.length > 0) {
+            dispatch(setEditProfile(profileValue))
+            dispatch(setIsProfile(true))
+        }
+    };
+
+    const handleProfileTextareaKeyDown = (e: any) => {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            handleSetProfile();
+        }
+    };
+    const handleAddComments = () => {
+        if (commentValue.length > 0) {
+            dispatch(addComments(commentValue))
+            setCommentValue('')
+        }
+    };
+
+    const handleCommentsTextareaKeyDown = (e: any) => {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            handleAddComments();
+        }
+    };
     const adjustTextareaAddProfileHeight = () => {
         let textarea = document.getElementById("profile");
         if (textarea) {
@@ -62,107 +88,87 @@ const EditModeComponent = () => {
         };
     }, [profileValue, commentValue, nameValue]);
     console.log(nameValue)
-    const handleAddComment = () => {
-        dispatch(addComments(commentValue))
-        setCommentValue('')
-    }
-    const handleAddProfile = () => {
-        if (profileValue && profileValue.length > 0) {
-            dispatch(setEditProfile(profileValue))
-            dispatch(setIsProfile(true))
-        }
-    }
-    const handleTextareaKeyDown = (e: any) => {
-        if (e.key === 'Enter') {
-            e.preventDefault();
-            handleAddProfile();
-        }
-        const handleTextareaKeyDown = (e: any) => {
-            if (e.key === 'Enter') {
-                e.preventDefault();
-            }
-        }
-        return (
-            <div className={s.editModBackground}>
-                <div className={s.editBackground}>
-                    <div className={s.editWindow}>
-                        <img draggable={false} onClick={() => {
-                            dispatch(editList())
-                            dispatch(setIsEditMode(false))
-                            dispatch(setEditElement(null))
-                        }} className={s.closeImg} src={close} alt="" />
-                        {/* header */}
-                        <div className={s.header}>
-                            <img draggable={false} className={s.cartNameImg} src={kartName} alt="" />
-                            {
-                                isEditName
-                                    ? <textarea autoFocus={true} ref={textareaRef} onFocus={adjustTextareaEditNameHeight} value={nameValue} onChange={(e) => setNameValue(e.currentTarget.value)} className={s.nameTextArea}></textarea>
-                                    : <div onClick={() => setIsEditName(true)} className={s.cartName}>{editElement && editElement.item.text}</div>
 
-                            }
-                            <div className={s.cartList}>у списку: {editElement && todos[editElement.boardIndex].title}</div>
-                        </div>
-                        {/* content */}
-                        <div className={s.cartContent}>
-                            <div className={s.cartProfile}>
-                                <img draggable={false} className={s.stringImg} src={string} alt="" />
-                                <div className={s.cartProfileName}>
-                                    Опис
-                                </div>
-                                <div >
-                                    {editElement && editElement.item.profile.length > 0 && isProfile
-                                        ? <div onClick={() => dispatch(setIsProfile(false))} className={s.profile}>{profileValue}</div>
+    return (
+        <div className={s.editModBackground}>
+            <div className={s.editBackground}>
+                <div className={s.editWindow}>
+                    <img draggable={false} onClick={() => {
+                        dispatch(editList())
+                        dispatch(setIsEditMode(false))
+                        dispatch(setEditElement(null))
+                    }} className={s.closeImg} src={close} alt="" />
+                    {/* header */}
+                    <div className={s.header}>
+                        <img draggable={false} className={s.cartNameImg} src={kartName} alt="" />
+                        {
+                            isEditName
+                                ? <textarea autoFocus={true} ref={textareaRef} onFocus={adjustTextareaEditNameHeight} value={nameValue} onChange={(e) => setNameValue(e.currentTarget.value)} className={s.nameTextArea}></textarea>
+                                : <div onClick={() => setIsEditName(true)} className={s.cartName}>{editElement && editElement.item.text}</div>
 
-                                        : <div>
-                                            <div ref={profileRef}>
-                                                < textarea onKeyDown={handleAddProfile} value={profileValue} onChange={(e) => setProfileValue(e.currentTarget.value)} onInput={adjustTextareaAddProfileHeight} id='profile' placeholder='Додати детальний опис...' className={s.cartProfileTextarea}></textarea>
-                                                {profileValue && profileValue.length > 0 &&
-                                                    <div className={s.buttonBlock}>
-                                                        <button onClick={() => {
-                                                            if (profileValue.length > 0) {
-                                                                dispatch(setEditProfile(profileValue))
-                                                                dispatch(setIsProfile(true))
-                                                            }
-                                                        }} className={s.buttonAdd}>Зберегти</button>
-                                                        <button onClick={() => setProfileValue('')} className={s.buttonNoAdd}>Відмінити</button>
-                                                    </div>}
-                                            </div>
-
-                                        </div>
-                                    }
-                                </div>
-
+                        }
+                        <div className={s.cartList}>у списку: {editElement && todos[editElement.boardIndex].title}</div>
+                    </div>
+                    {/* content */}
+                    <div className={s.cartContent}>
+                        <div className={s.cartProfile}>
+                            <img draggable={false} className={s.stringImg} src={string} alt="" />
+                            <div className={s.cartProfileName}>
+                                Опис
                             </div>
-                            <div className={s.cartComment}>
-                                <img draggable={false} className={s.commImg} src={comm} alt="" />
-                                <div className={s.cartCommName}>
-                                    Активність
-                                </div>
-                                <div ref={commentRef}>
-                                    <textarea onKeyDown={handleTextareaKeyDown} value={commentValue} onChange={(e) => setCommentValue(e.currentTarget.value)} onInput={adjustTextareaAddCommentHeight} id='comment' placeholder='Написати коментар...' className={s.cartCommTextarea}></textarea>
-                                    {commentValue.length > 0 &&
-                                        <div className={s.buttonBlock}>
-                                            <button
+                            <div >
+                                {editElement && editElement.item.profile.length > 0 && isProfile
+                                    ? <div onClick={() => dispatch(setIsProfile(false))} className={s.profile}>{profileValue}</div>
 
-                                                onClick={() => {
-                                                    dispatch(addComments(commentValue))
-                                                    setCommentValue('')
-                                                }} className={s.buttonAdd}>Зберегти</button>
-                                            <button onClick={() => setCommentValue('')} className={s.buttonNoAdd}>Відмінити</button>
+                                    : <div>
+                                        <div ref={profileRef}>
+                                            < textarea onKeyDown={handleProfileTextareaKeyDown} value={profileValue} onChange={(e) => setProfileValue(e.currentTarget.value)} onInput={adjustTextareaAddProfileHeight} id='profile' placeholder='Додати детальний опис...' className={s.cartProfileTextarea}></textarea>
+                                            {profileValue && profileValue.length > 0 &&
+                                                <div className={s.buttonBlock}>
+                                                    <button onClick={() => {
+                                                        if (profileValue.length > 0) {
+                                                            dispatch(setEditProfile(profileValue))
+                                                            dispatch(setIsProfile(true))
+                                                        }
+                                                    }} className={s.buttonAdd}>Зберегти</button>
+                                                    <button onClick={() => setProfileValue('')} className={s.buttonNoAdd}>Відмінити</button>
+                                                </div>}
                                         </div>
-                                    }
-                                </div>
-                                <div className={s.comments}>
-                                    {editElement?.item.comments.map((text, i) =>
-                                        <CommentComponent text={text} index={i} />
-                                    )}
-                                </div>
+
+                                    </div>
+                                }
+                            </div>
+
+                        </div>
+                        <div className={s.cartComment}>
+                            <img draggable={false} className={s.commImg} src={comm} alt="" />
+                            <div className={s.cartCommName}>
+                                Активність
+                            </div>
+                            <div ref={commentRef}>
+                                <textarea onKeyDown={handleCommentsTextareaKeyDown} value={commentValue} onChange={(e) => setCommentValue(e.currentTarget.value)} onInput={adjustTextareaAddCommentHeight} id='comment' placeholder='Написати коментар...' className={s.cartCommTextarea}></textarea>
+                                {commentValue.length > 0 &&
+                                    <div className={s.buttonBlock}>
+                                        <button
+
+                                            onClick={() => {
+                                                dispatch(addComments(commentValue))
+                                                setCommentValue('')
+                                            }} className={s.buttonAdd}>Зберегти</button>
+                                        <button onClick={() => setCommentValue('')} className={s.buttonNoAdd}>Відмінити</button>
+                                    </div>
+                                }
+                            </div>
+                            <div className={s.comments}>
+                                {editElement?.item.comments.map((text, i) =>
+                                    <CommentComponent text={text} index={i} />
+                                )}
                             </div>
                         </div>
                     </div>
                 </div>
-            </div >
-        )
-    }
+            </div>
+        </div >
+    )
 }
 export default EditModeComponent
